33f441e69bc983847de3220463c2f74e924ad53b
Vict0rrrr <v.rouviere@myskolae.fr>
2025-11-13 08:26:08 +0100

    auto

--- Terraform/test-deploy.tf
+++ Terraform/test-deploy.tf
@@ -3,7 +3,7 @@ resource "proxmox_vm_qemu" "debian_test" {
   target_node = "PROXMOX-PVE1"
   clone       = "debian-12-template-cloud"
   full_clone  = true
-}
+
 
  # Configuration matérielle
   cores       = 1
@@ -34,3 +34,4 @@ resource "proxmox_vm_qemu" "debian_test" {
 
    # DHCP automatique
   ipconfig0 = "ip=dhcp"
+}
