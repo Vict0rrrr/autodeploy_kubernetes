1efb691a87f1d3b51df60289c901a5cc0f2f0cfd
Vict0rrrr <v.rouviere@myskolae.fr>
2025-11-13 23:50:04 +0100

    auto

--- Terraform/test-deploy.tf
+++ Terraform/test-deploy.tf
@@ -1,38 +1,63 @@
-resource "proxmox_vm_qemu" "debian_test" {
-  name        = "debian-test-01"
+resource "proxmox_vm_qemu" "cloudinit-example" {
+  vmid        = 101
+  name        = "test-terraform0"
   target_node = "PROXMOX-PVE1"
-  clone       = "debian-12-template-cloud"
-  full_clone  = true
+  agent       = 1
+  cores       = 2
+  memory      = 1024
+  boot        = "order=scsi0" # has to be the same as the OS disk of the template
+  clone       = "debian13-cloudinit" # The name of the template
+  scsihw      = "virtio-scsi-single"
+  vm_state    = "running"
+  automatic_reboot = true
 
+  # Cloud-Init configuration
+  cicustom   = "vendor=local:snippets/qemu-guest-agent.yml" # /var/lib/vz/snippets/qemu-guest-agent.yml
+  ciupgrade  = true
+  nameserver = "1.1.1.1 8.8.8.8"
+  ipconfig0  = "ip=dhcp
+  skip_ipv6  = true
+  ciuser     = "root"
+  cipassword = "root"
 
- # Configuration matérielle
-  cores       = 1
-  sockets     = 1
-  memory      = 2048
-  scsihw      = "virtio-scsi-pci"
-  bootdisk    = "scsi0"
-
-  disk {
-    slot     = 0
-    size     = "50G"
-    type     = "scsi"
-    storage  = "SSD-PVE-DATA"
-    iothread = true
-  
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
   }
 
-  # Réseau
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "SSD-PVE-DATA"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size    = "30G" 
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "SSD-PVE-DATA"
+        }
+      }
+    }
+  }
+
   network {
     id = 0
-    model  = "virtio"
     bridge = "vmbr1"
-    tag    = 40
+    model  = "virtio,tag=40"
   }
+}
 
-  # Cloud-init
-  ciuser      = "debian"
-  cipassword  = "debian"
-
-   # DHCP automatique
-  ipconfig0 = "ip=dhcp"
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "3.0.1rc4"
+    }
+  }
 }
